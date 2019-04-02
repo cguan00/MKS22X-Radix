b0c7ffe30d7eee108fa42c1e242c7db58d38049c
Christy Guan <cguan00@stuy.edu>
2019-04-01 21:38:11 -0400

    created buckets array and a bucket for each digit

--- Radix.java
+++ Radix.java
@@ -1,13 +1,20 @@
 public class Radix{
-  public static void radixsort(int[] data){
-    // MyLinkedList<Integer> buckets = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> zero = new MyLinkedList<Integer>();
 
-    for(int i = 0; i < data.length; i++){
-      int num = data[i];
-      int digit = num % 10;//change later to reflect the number of passes you have to go through. use another loop
-      zero.add(num);
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  public static void radixsort(int[] data){
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+
+    //determining the largest number in the data array
+    int max = 0;
+    for(int i = 0; i < data.length; i++) {
+      max = Math.max(max, data[i]);
     }
+
+    //created a MyLinkedList bucket for each digit
+    for(int i = 0; i < 10; i++){
+      buckets[i] = new MyLinkedList();
+    }
+
 
 
   }

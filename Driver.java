1ec56abb7c8ade65a5807e915ae4a46dd8601600
Christy Guan <cguan00@stuy.edu>
2019-04-02 14:46:49 -0400

    simple tests

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+  public static void main(String[] args) {
+    // int[] data1 = new int[] {10, 4, 321, 75, 64};
+    // Radix.radixsort(data1);
+
+    int[] data2 = new int[] {51, 82, 73, 94, 25, 36};
+    Radix.radixsort(data2);
+  }
+
+}

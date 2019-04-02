7776b1ddfba58b2075dd131de74a93f870fcac41
Christy Guan <cguan00@stuy.edu>
2019-04-01 23:00:58 -0400

    started on digit method, doesn't work

--- Radix.java
+++ Radix.java
@@ -19,18 +19,35 @@ public class Radix{
     }
     int passes = (int)Math.log10(max) + 1;//number of passes is number of digits in max value
 
+    //special case?
+    if(max == 0){
+      passes = 1;
+    }
+  }
 
-
+  //parameters are the digit and the position of the digit you want
+  //ex. digit(23,0) would return 3, digit(23,1) would return 2
+  public static int digit(int num, int pos){
+    if(pos == 0){
+      return num / 10;
+    }
+    return (int) (num % Math.pow(10, pos));
   }
 
   public static void main(String[] args) {
-    System.out.println((int)Math.log10(5) + 1);//1 digit
-    System.out.println((int)Math.log10(3) + 1);//1 digit
-    System.out.println((int)Math.log10(7) + 1);//1 digit
-    // System.out.println((int)Math.log10(0));
-    System.out.println((int)Math.log10(56) + 1);//2 digit
-    System.out.println((int)Math.log10(45) + 1);//2 digit
-    System.out.println((int)Math.log10(450) + 1);//3 digit
+    // System.out.println((int)Math.log10(5) + 1);//1 digit
+    // System.out.println((int)Math.log10(3) + 1);//1 digit
+    // System.out.println((int)Math.log10(7) + 1);//1 digit
+    // System.out.println((int)Math.log10(0) + 1);//weird number
+    // System.out.println((int)Math.log10(56) + 1);//2 digit
+    // System.out.println((int)Math.log10(45) + 1);//2 digit
+    // System.out.println((int)Math.log10(450) + 1);//3 digit
+
+    System.out.println(digit(23,0));
+    System.out.println(digit(23,1));
+
+    // System.out.println(23 % 10);
+    // System.out.println(23 % 100);
   }
 
 }
